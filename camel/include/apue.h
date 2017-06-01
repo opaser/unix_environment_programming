58fc8cdb6fdf6e03fcc7d51c30e4739e1598f276
opaser <1017370773@qq.com>
2017-06-01 11:59:38 +0800

    ls

---
+++ camel/include/apue.h
@@ -0,0 +1,15 @@
+#ifndef _APUE_H
+#define _APUE_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
+#include <unistd.h>
+#include <signal.h>
+
+#define MAXLINE 4096
+
+/* error.c */
+void  err_quit(const char *, ...) __attribute__((noreturn));
+void  err_sys(const char *, ...)__attribute__((noreturn));
+#endif
