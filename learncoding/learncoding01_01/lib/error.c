0a3bcda882e65b5d278d888eb14db460158f338b
opaser <1017370773@qq.com>
2017-06-01 12:35:55 +0800

    change location

--- learncoding/learncoding01_01/lib/error.c
+++ learncoding/learncoding01_01/lib/error.c
@@ -11,6 +11,15 @@ void err_quit(const char *fmt, ...)
 	exit(1);
 }
 
+void err_sys(const char *fmt, ...)
+{
+	va_list ap;
+	va_start(ap, fmt);
+	err_doit(1, errno, fmt, ap);
+	va_end(ap);
+	exit(1);
+}
+
 static void err_doit(int errnoflag, int error, const char *fmt, va_list ap)
 {
 	char buf[MAXLINE];
@@ -23,11 +32,3 @@ static void err_doit(int errnoflag, int error, const char *fmt, va_list ap)
 	fflush(NULL);
 }
 
-void err_sys(const char *fmt, ...)
-{
-	va_list ap;
-	va_start(ap, fmt);
-	err_doit(1, errno, fmt, ap);
-	va_end(ap);
-	exit(1);
-}
